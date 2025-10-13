cac433c93496cdd8e9939d42f0ac9c8d0cb18fcb
Josh Dales <git@joshdales.dev>
2025-10-13 15:04:15 -0400

    Convert json file to regular ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { generate as vscode } from "./templates/vscode/build.ts";
 import { generate as ghostty } from "./templates/ghostty/build.ts";
 import { generate as warp } from "./templates/warp/build.ts";
 import { generate as zed } from "./templates/zed/build.ts";
-import taigaDark from "./colours/taiga-dark.json" with { type: "json" };
-import taigaLight from "./colours/taiga-light.json" with { type: "json" };
+import * as taigaDark from './colours/taiga-dark.ts'
+import * as taigaLight from './colours/taiga-light.ts'
 
 const packagesDir = resolve(import.meta.dirname, "..", "packages");
-const colours = [taigaDark, taigaLight];
+const colours = [
+	taigaDark.taiga, taigaLight.taigaLight,
+];
 
 function run() {
 	colours.forEach((colourSet) => {
